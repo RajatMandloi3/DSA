ea8c71a1c2ed321d46c407c1de7062d6a5fba426
Rajat Mandloi <rajatmandloi@Rajats-MacBook-Air.local>
2025-05-20 21:57:02 +0530

    feat: main branch

--- Binary-search/findTotalOccurance.js
+++
@@ -1,9 +0,0 @@
-const { findFirstOccuranceOfNumber } = require('./findFirstOccuranceOfNumber')
-const { findLastOccuranceOfNumber } = require('./findLastOccurance')
-
-const findTotalOccu = (arr, target) => {
-  const firstOccurance = findFirstOccuranceOfNumber(arr, target)
-  const lastOccurance = findLastOccuranceOfNumber(arr, target)
-  return lastOccurance - firstOccurance + 1
-}
-console.log('total', findTotalOccu([1, 2, 3, 4, 6, 6, 6, 6, 6, 7], 6))
--- bitwiseOperators.js
+++
@@ -1,7 +0,0 @@
-// console.log(2&3);
-// console.log(2|3);
-// console.log(2^3^4^5^3^4^2)
-// console.log(~1)
-
-console.log(10 << 4)
-console.log(5 >> 1)
